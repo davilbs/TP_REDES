6ac884f9e8346d126c95699b03a1a117f25a3d5d
davilbs <davilage.borges@gmail.com>
2023-05-09 20:05:13 -0300

    Added IPv6 support

--- client/client.c
+++ client/client.c
@@ -8,30 +8,47 @@ int main(int argc, char *argv[])
 
     char *address = argv[1];
     int ipv = checkVersion(address);
-    int proto = (ipv == 2) ? 0 : 41;
     if (ipv == 0)
         DieUsrError("Invalid address format", "Use IPv4 or IPv6 format");
 
     // Stablish connection
-    int sock = socket(ipv, SOCK_STREAM, proto);
+    int sock = socket(ipv, SOCK_STREAM, 0);
     if (sock < 0)
         DieSysError("failed to open socket");
-
-    struct sockaddr_in servAddr;
-    memset(&servAddr, 0, sizeof(servAddr));
-    servAddr.sin_family = ipv;
-
-    int rtnVal = inet_pton(ipv, address, &servAddr.sin_addr.s_addr);
-    if (rtnVal == 0)
-        DieUsrError("Ip string to address failed", "Invalid address string");
-    else if (rtnVal < 0)
-        DieSysError("Ip string to address failed");
-
+    
     in_port_t port = atoi(argv[2]);
-    servAddr.sin_port = htons(port);
+    if(ipv == 2)
+    {
+        struct sockaddr_in servAddr;
+        memset(&servAddr, 0, sizeof(servAddr));
+        servAddr.sin_family = ipv;
 
-    if (connect(sock, (struct sockaddr *)&servAddr, sizeof(servAddr)) < 0)
-        DieSysError("Connection failed");
+        int rtnVal = inet_pton(ipv, address, &servAddr.sin_addr.s_addr);
+        if (rtnVal == 0)
+            DieUsrError("Ip string to address failed", "Invalid address string");
+        else if (rtnVal < 0)
+            DieSysError("Ip string to address failed");
+        servAddr.sin_port = htons(port);
+        if (connect(sock, (struct sockaddr *)&servAddr, sizeof(servAddr)) < 0)
+            DieSysError("Connection failed");
+    }else {
+        struct sockaddr_in6 servAddr;
+        memset(&servAddr, 0, sizeof(servAddr));
+        servAddr.sin6_family = ipv;
+
+        int rtnVal = inet_pton(ipv, address, &servAddr.sin6_addr);
+        if (rtnVal == 0)
+            DieUsrError("Ip string to address failed", "Invalid address string");
+        else if (rtnVal < 0)
+            DieSysError("Ip string to address failed");
+            
+        servAddr.sin6_port = htons(port);
+        if (connect(sock, (struct sockaddr *)&servAddr, sizeof(servAddr)) < 0)
+            DieSysError("Connection failed");
+    }
+
+
+
 
     // Main client loop
     char *filename = NULL;
@@ -39,7 +56,6 @@ int main(int argc, char *argv[])
     for (;;)
     {
         filename = cmdParse(sock);
-
         fp = fopen(filename, "r");
         while (sendFile(fp, filename, sock) > 0);
         fclose(fp);
--- server/server.c
+++ server/server.c
@@ -10,53 +10,80 @@ int main(int argc, char *argv[])
     in_port_t port = atoi(argv[2]);
 
     // Check IP version
+    // Open network socket
     int servSock;
-    int af, proto, addrany;
     if (strcmp(version, "v4") == 0)
     {
-        af = AF_INET;
-        proto = IPPROTO_IP;
+        servSock = socket(AF_INET, SOCK_STREAM, 0);
+        if (servSock < 0)
+            DieSysError("Failed to open socket");
+
+        struct sockaddr_in servAddr;
+        memset(&servAddr, 0, sizeof(servAddr));
+        servAddr.sin_family = AF_INET;
+        servAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+        servAddr.sin_port = htons(port);
+
+        if (bind(servSock, (struct sockaddr *)&servAddr, sizeof(servAddr)) < 0)
+            DieSysError("Failed to bind to socket");
     }
     else if (strcmp(version, "v6") == 0)
     {
-        af = AF_INET6;
-        proto = IPPROTO_IPV6;
+        servSock = socket(AF_INET6, SOCK_STREAM, 0);
+        if (servSock < 0)
+            DieSysError("Failed to open socket");
+
+        struct sockaddr_in6 servAddr;
+        memset(&servAddr, 0, sizeof(servAddr));
+        servAddr.sin6_family = AF_INET6;
+        servAddr.sin6_addr = in6addr_any;
+        servAddr.sin6_port = htons(port);
+
+        if (bind(servSock, (struct sockaddr *)&servAddr, sizeof(servAddr)) < 0)
+            DieSysError("Failed to bind to socket");
     }
     else
         DieUsrError("Invalid IP version", "use v4 or v6");
-    
-    // Open network socket
-    servSock = socket(af, SOCK_STREAM, proto);
-    if (servSock < 0)
-        DieSysError("Failed to open socket");
-
-    struct sockaddr_in servAddr;
-    memset(&servAddr, 0, sizeof(servAddr));
-    servAddr.sin_family = af;
-    servAddr.sin_addr.s_addr = htonl(INADDR_ANY);
-    servAddr.sin_port = htons(port);
-
-    if (bind(servSock, (struct sockaddr *)&servAddr, sizeof(servAddr)) < 0)
-        DieSysError("Failed to bind to socket");
 
     if (listen(servSock, MAXPENDING) < 0)
         DieSysError("Failed to listen on port");
 
     // Main server loop
-    for (;;)
+    if (strcmp(version, "v4") == 0)
     {
-        struct sockaddr_in clntAddr;
-        socklen_t clntAddrLen = sizeof(clntAddr);
+        for (;;)
+        {
+            struct sockaddr_in clntAddr;
+            socklen_t clntAddrLen = sizeof(clntAddr);
 
-        int clntSock = accept(servSock, (struct sockaddr *)&clntAddr, &clntAddrLen);
-        if (clntSock < 0)
-            DieSysError("Failed to accept connection");
+            int clntSock = accept(servSock, (struct sockaddr *)&clntAddr, &clntAddrLen);
+            if (clntSock < 0)
+                DieSysError("Failed to accept connection");
 
-        char clntName[INET_ADDRSTRLEN];
-        if (inet_ntop(af, &clntAddr.sin_addr.s_addr, clntName, sizeof(clntName)) != NULL)
-            handleClient(clntSock);
-        else
-            puts("Unable to get client address");
+            char clntName[INET_ADDRSTRLEN];
+            if (inet_ntop(AF_INET, &clntAddr.sin_addr.s_addr, clntName, sizeof(clntName)) != NULL)
+                handleClient(clntSock);
+            else
+                puts("Unable to get client address");
+        }
+    }
+    else
+    {
+        for (;;)
+        {
+            struct sockaddr_in6 clntAddr;
+            socklen_t clntAddrLen = sizeof(clntAddr);
+
+            int clntSock = accept(servSock, (struct sockaddr *)&clntAddr, &clntAddrLen);
+            if (clntSock < 0)
+                DieSysError("Failed to accept connection");
+
+            char clntName[INET_ADDRSTRLEN];
+            if (inet_ntop(AF_INET6, &clntAddr.sin6_addr, clntName, sizeof(clntName)) != NULL)
+                handleClient(clntSock);
+            else
+                puts("Unable to get client address");
+        }
     }
 
     close(servSock);
